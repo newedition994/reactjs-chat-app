76e325c53d526d90544ac0a956cfea39259aae1e
Breyanna Vass <breyannavass@gmail.com>
2019-06-03 15:49:27 -0400

    Importing messages in MessageList

---
+++ src/components/MessageList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Message from './Message'
+
+class MessageList extends Component {
+    render() {
+        return (
+            <div>
+                <Message />
+            </div>
+        )
+    }
+}
+
+export default MessageList;
