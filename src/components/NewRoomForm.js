05295b84a713887296e8a6adea344d7d729de193
Breyanna Vass <breyannavass@gmail.com>
2019-06-10 11:26:34 -0400

    Create the new room form

--- src/components/NewRoomForm.js
+++ src/components/NewRoomForm.js
@@ -4,7 +4,12 @@ class NewRoomForm extends React.Component {
     render() {
         return (
             <div className="new-room-form">
-
+                <form>
+                    <input type="text"
+                        placeholder="NewRoomForm"
+                        required />
+                    <button id="create-room-btn" type="submit">+</button>
+                </form>
             </div>
 
         )
