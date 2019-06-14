702b0bd1052ed1355e75bff6f16bbfcac91e9122
Breyanna Vass <breyannavass@gmail.com>
2019-06-14 11:02:29 -0400

    Adding state to the Appjs component

--- src/App.js
+++ src/App.js
@@ -10,22 +10,32 @@ import { tokenUrl, instanceLocator } from './config'
 
 class App extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      messages: []
+    }
+  }
+
   componentDidMount() {
-    const ChatManager = new Chatkit.ChatManager({
+    const chatManager = new Chatkit.ChatManager({
       instanceLocator,
-      userId: 'nancydrw89',
+      userId: 'marrkymark78',
       tokenProvider: new Chatkit.TokenProvider({
         url: tokenUrl
       })
     })
 
-    ChatManager.connect()
+    chatManager.connect()
       .then(currentUser => {
         currentUser.subscribeToRoom({
           roomId: 19887298,
           hooks: {
             onNewMessage: message => {
               console.log('message.text', message.text);
+              this.setState({
+                messages: [...this.state.messages, message]
+              })
             }
           }
         })
