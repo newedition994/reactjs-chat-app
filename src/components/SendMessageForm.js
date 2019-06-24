b3d48a9cc90173cad3e7948fd430088d9f32b7ff
Breyanna Vass <breyannavass@gmail.com>
2019-06-24 09:29:00 -0400

    Formatting the sendMessageForm

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,11 +1,33 @@
 import React from 'react'
 
 class SendMessageForm extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            message: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(e) {
+        console.log(e.target.value)
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <form className="send-message-form">
+            <form onSubmit={this.handleSubmit} className="send-message-form">
                 <input
-                    placeholder="SendMessageForm"
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Type your message and hit Enter"
                     type="text" />
             </form>
         )
